cfe8f131ef37551d4181f90ffcb3dd1a7f0d2522
kirubel demelash <62418642+kiztopia1@users.noreply.github.com>
2023-02-09 15:09:09 +0300

    added

--- index.js
+++ index.js
@@ -9,6 +9,73 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+// addBot
+app.post('/addBot/:id', function(req, res, next) {
+  const id = req.params.id ;
+
+  const bot = new Bot({
+      id: id, 
+      name: req.body.name,
+  })
+  bot.save()
+  .then(bot => {
+
+      res.end();
+  })
+});
+
+// add command
+app.post('/addCommand/:id', function(req, res, next) {
+  const id = req.params.id ;
+  console.log(id)
+  Bot.findOneAndUpdate({id: id}, {command: req.body.command})
+  .then(response => {
+
+      res.end()
+  })
+});
+
+// addResponse
+app.post('/addResponse/:id', function(req, res, next) {
+  const id = req.params.id ;
+  console.log(id)
+  Bot.findOneAndUpdate({id: id}, {response: req.body.response})
+  .then(response => {
+
+      res.end()
+  })
+});
+
+//bot
+app.get('/bot/:id', function(req, res, next) {
+  const id = req.params.id ;
+  Bot.findOne({id: id})
+  .then(bot => {
+    res.json({"response": bot})
+    })
+});
+
+//bots
+app.get('/bots', function(req, res, next) {
+  Bot.find()
+  .then(bots => {
+    res.json({"response": bots})
+  })
+});
+
+
+// getCommands
+app.get('/getCommands/:id', function(req, res, next) {
+  const id = req.params.id ;
+  console.log(id)
+  Bot.findOne({id: id})
+  .then(response => {
+
+      res.json({"response": response.command})
+  })
+});
+
+
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
