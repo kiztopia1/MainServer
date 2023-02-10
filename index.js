081c252071065410643f5faa8a4ff5ccc0f62cf9
kirubel demelash <62418642+kiztopia1@users.noreply.github.com>
2023-02-10 14:02:31 +0300

    ejs deleted

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const multer = require('multer');
-var path = require('path');
 // Initialize Express
 const app = express();
 app.use(express.json());
@@ -11,9 +10,6 @@ const Bot = require('./models/bot');
 const Shot = require('./models/image')
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 // multer setup
 const storage   = multer.diskStorage({
@@ -132,7 +128,7 @@ app.get('/bots', function(req, res, next) {
     .then(() => {
       Bot.find()
   .then(bots => {
-    res.render( 'bots',{ title: 'bots', "bots": bots });
+    res.json({"response": bots})
   })
     })
     
