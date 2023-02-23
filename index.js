165e42bcbd622d59506d230b4b60323c57cc24f4
kirubel demelash <62418642+kiztopia1@users.noreply.github.com>
2023-02-23 06:26:59 +0300

    test

--- index.js
+++ index.js
@@ -94,44 +94,26 @@ app.post("/addResponse/:id", function (req, res, next) {
         fs.readFile(
           path.join(__dirname + "../../../tmp/" + req.file.filename),
           "utf8",
-          function (err, data) {
+          async function (err, data) {
             if (err) throw err;
-
-            txt = data;
+            await mongoose
+              .connect(
+                "mongodb+srv://shepherd:6322@cluster0.xow6jeh.mongodb.net/?retryWrites=true&w=majority"
+              )
+              .then((dbRes) => {
+                Bot.findOneAndUpdate(
+                  { id: req.params.id },
+                  { response: data }
+                ).then(() => {
+                  res.send(txt);
+                });
+              });
           }
         );
-
-        await mongoose
-          .connect(
-            "mongodb+srv://shepherd:6322@cluster0.xow6jeh.mongodb.net/?retryWrites=true&w=majority"
-          )
-          .then((dbRes) => {
-            Bot.findOneAndUpdate({ id: req.params.id }, { response: txt }).then(
-              () => {
-                res.send(txt);
-              }
-            );
-          });
       }
       connect();
     }
   });
-
-  // console.log(req.file)
-  // async function connect() {
-  //   await mongoose.connect('mongodb+srv://shepherd:6322@cluster0.xow6jeh.mongodb.net/?retryWrites=true&w=majority')
-  //   .then(() => {
-  //     Bot.findOneAndUpdate({id: id}, {response: req.body.response})
-  // .then(() => {
-
-  //   res.send("success");
-  // })
-  //   })
-
-  // }
-  //  connect();
-
-  // res.end()
 });
 
 //bot
